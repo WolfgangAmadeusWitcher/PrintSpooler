f1f54001302171dbead9ac9cab421f837f46a541
WolfgangAmadeusWitcher <67244083+WolfgangAmadeusWitcher@users.noreply.github.com>
2020-10-08 11:34:42 +0300

    Add project files.

---
+++ PrintSpoolerCrossPlatform/Program.cs
@@ -0,0 +1,21 @@
+﻿using Atom8.Monitors;
+using System;
+
+namespace PrintSpoolerCrossPlatform
+{
+    class Program
+    {
+        static PrintQueueMonitor pqm = null;
+        static void Main(string[] args)
+        {
+            string printerName = "";
+            pqm = new PrintQueueMonitor(printerName);
+            pqm.OnJobStatusChange += new PrintJobStatusChanged(pqm_OnJobStatusChange);
+        }
+
+        static void pqm_OnJobStatusChange(object Sender, PrintJobChangeEventArgs e)
+        {
+            Console.WriteLine(e.JobID + " - " + e.JobName + " - " + e.JobStatus);
+        }
+    }
+}
